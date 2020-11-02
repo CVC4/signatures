96306fb1938f449fd6e3beeae42c0659198e683e
yoni206 <yoni206@gmail.com>
2020-11-02 08:58:17 -0800

    script

---
+++ lfsc/new/scripts/simple.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+MY_DIR="`dirname \"$0\"`"
+cd $MY_DIR
+cd ../signatures
+lfscc \
+  core_defs.plf \
+  theory_def.plf \
+  type_checking_programs.plf \
+  boolean_rules.plf \
+  cnf_rules.plf \
+  equality_rules.plf \
+  quantifiers_rules.plf \
+  ../proofs/equality_proofs.plf \
+  ../proofs/quantifiers_proofs.plf \
+
+cd -
