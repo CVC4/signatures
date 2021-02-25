c37c6737e2c562b219f7c95507d5cc6d65076413
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-02-25 20:11:09 -0300

    adding non-clausal equality rules

---
+++ lean/euf.lean
@@ -0,0 +1,26 @@
+import cdclt
+
+open proof
+open proof.sort proof.term
+open rules
+
+namespace eufRules
+
+/-*************** congruence ***************-/
+
+constant refl {t : option term} : thHolds $ mkEq t t
+
+constant symm : Π {t₁ t₂ : option term},
+  thHolds (mkEq t₁ t₂) → thHolds (mkEq t₂ t₁)
+
+constant trans : Π {t₁ t₂ t₃ : option term},
+  thHolds (mkEq t₁ t₂) → thHolds (mkEq t₂ t₃) → thHolds (mkEq t₁ t₃)
+
+constant cong : Π {t₁ t₂ : option term} (f : term),
+  thHolds (mkEq t₁ t₂) → thHolds (mkEq (mkApp f t₁) (mkApp f t₂))
+
+constant congHO : Π {f₁ t₁ : option term} {f₂ t₂ : option term},
+  thHolds (mkEq f₁ f₂) → thHolds (mkEq t₁ t₂) →
+        thHolds (mkEq (mkApp f₁ t₁) (mkApp f₂ t₂))
+
+end eufRules
---
+++ lean/exampleEUF.lean
@@ -0,0 +1,29 @@
+import euf
+
+open proof
+open proof.sort proof.term
+open rules
+open eufRules
+
+def U := atom 0
+def a₁ := const 0 U
+def a₂ := const 1 U
+def a₃ := const 2 U
+def b₁ := const 3 U
+def b₂ := const 4 U
+def f₁ := const 5 (mkArrowN [U, U, U])
+def f₂ := const 6 (mkArrowN [U, U, U])
+def f₃ := const 7 (mkArrowN [U, U])
+
+noncomputable theorem binCong :
+  thHolds (mkEq a₁ a₂) → thHolds (mkEq b₁ b₂) → (thHolds (mkEq (mkApp (mkApp f₁ a₁) b₁) (mkApp (mkApp f₁ a₂) b₂))) :=
+assume s0 : thHolds (mkEq a₁ a₂),
+assume s1 : thHolds (mkEq b₁ b₂),
+show (thHolds (mkEq (mkApp (mkApp f₁ a₁) b₁) (mkApp (mkApp f₃ a₂) b₂))), from congHO (cong f₁ s0) s1
+
+noncomputable theorem rwCong :
+  thHolds (mkEq a₁ a₂) → thHolds (mkEq (mkEq (mkApp f₃ a₁) b₁) (mkEq (mkApp f₃ a₂) b₁)) :=
+assume s0 : thHolds (mkEq a₁ a₂),
+have s1 : thHolds (mkEq (mkApp f₃ a₁) (mkApp f₃ a₂)), from (cong f₃ s0),
+have s2 : thHolds (mkEq b₁ b₁), from refl,
+show thHolds (mkEq (mkEq (mkApp f₃ a₁) b₁) (mkEq (mkApp f₃ a₂) b₁)), from congHO (cong (const eq_num dep) s1) s2
