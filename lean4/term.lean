fe235c15faba4fc6abd0f9c1229c820cedfbfd28
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-03-10 00:01:39 -0300

    more lean4

--- lean4/term.lean
+++ lean4/term.lean
@@ -11,11 +11,12 @@ namespace proof
    Additionally, we have atomic sorts, parameterized by a nat, arrow
    for functional sorts, and bitvector sorts parameterized by their
    length -/
-inductive sort  : Type where
+inductive sort : Type where
 | dep : sort
 | atom : Nat → sort
 | arrow : sort → sort → sort
 | bv : Nat → sort
+deriving DecidableEq
 
 namespace sort
 
@@ -39,7 +40,7 @@ def bvNotNum : Nat := bvEqNum + 1
 def bvAndNum : Nat := bvNotNum + 1
 def bvOrNum : Nat := bvAndNum + 1
 
-def boolNum  : Nat := 0
+def boolNum : Nat := 1
 def intNum : Nat := boolNum + 1
 
 def sortToString : sort → String
@@ -65,6 +66,7 @@ end sort
 inductive value : Type where
 | bitvec : List Bool → value
 | integer : Int → value
+deriving DecidableEq
 
 def bvToString : List Bool → String
 | [] => ""
@@ -87,6 +89,7 @@ inductive term : Type where
 | const : Nat → Option sort → term
 | app : term → term → term
 | qforall : Nat → term → term
+deriving DecidableEq
 
 namespace term
 
@@ -96,8 +99,9 @@ open sort
 open value
 
 -- Sort definitions
-def boolSort := sort.atom boolNum
-def intSort := sort.atom intNum
+def wildcardSort := atom 0
+def boolSort := atom boolNum
+def intSort := atom intNum
 
 -- Interpreted constants
 def botConst := const botNum boolSort
@@ -116,27 +120,75 @@ def bvEqConst (n : Nat) :=
   const bvEqNum (arrow (bv n) (arrow (bv n) boolSort))
 
 -- macros for creating terms with interpreted constants
-def not : term → term := fun t => notConst • t
-def top : term := not botConst
-def or : term → term → term := fun t₁ t₂ => orConst • t₁ • t₂
-def and : term → term → term := fun t₁ t₂ => andConst • t₁ • t₂
-def implies : term → term → term := fun t₁ t₂ => impliesConst • t₁ • t₂
-def xor : term → term → term := fun t₁ t₂ => xorConst • t₁ • t₂
+@[matchPattern] def not : term → term := fun t => notConst • t
+@[matchPattern] def top : term := not botConst
+@[matchPattern] def or : term → term → term := fun t₁ t₂ => orConst • t₁ • t₂
+@[matchPattern] def and : term → term → term := fun t₁ t₂ => andConst • t₁ • t₂
+@[matchPattern] def implies : term → term → term :=
+  fun t₁ t₂ => impliesConst • t₁ • t₂
+@[matchPattern] def xor : term → term → term := fun t₁ t₂ => xorConst • t₁ • t₂
 
-def fIte : term → term → term → term := fun t₁ t₂ t₃ => fIteConst • t₁ • t₂ • t₃
-def eq : term → term → term := fun t₁ t₂ => eqConst • t₁ • t₂
+@[matchPattern] def fIte : term → term → term → term :=
+  fun t₁ t₂ t₃ => fIteConst • t₁ • t₂ • t₃
+@[matchPattern] def eq : term → term → term := fun t₁ t₂ => eqConst • t₁ • t₂
 
-def bitOf : Nat → term → term → term := fun n t₁ t₂ => bitOfConst n • t₁ • t₂
-def bvEq : Nat → term → term → term := fun n t₁ t₂ => bvEqConst n • t₁ • t₂
+@[matchPattern] def bitOf : Nat → term → term → term :=
+  fun n t₁ t₂ => bitOfConst n • t₁ • t₂
+@[matchPattern] def bvEq : Nat → term → term → term :=
+  fun n t₁ t₂ => bvEqConst n • t₁ • t₂
 
 -- computing the sort of terms
 def sortOfAux : term → Option sort
 | val (integer i) _ => intSort
 | val (bitvec l) _ =>
-  do
-  let len ← List.length l
-  if len = 0 then none else bv len
-| _ => boolSort
+    do let len ← List.length l if len = 0 then none else bv len
+| eq t₁ t₂ =>
+    sortOfAux t₁ >>= fun s₁ =>
+    sortOfAux t₂ >>= fun s₂ =>
+    if s₁ = s₂ then boolSort else none
+| fIte c t₁ t₂ =>
+    sortOfAux c >>= fun s₁ =>
+    sortOfAux t₁ >>= fun s₂ =>
+    sortOfAux t₂ >>= fun s₃ =>
+    if s₁ = boolSort ∧ s₂ = s₃ then s₂ else none
+| f • t =>
+    sortOfAux f >>= fun sf =>
+    sortOfAux t >>= fun st =>
+    match sf with
+    | arrow st s => s
+    | _ => none
+| qforall v t =>
+    sortOfAux t >>= fun st => if st = boolSort then st else none
+-- parametric operators are by default well-solted, with a special default type
+| const _ dep => wildcardSort
+| const _ s => s
+
+def sortOf (t : Option term) : Option sort := t >>= fun t' => sortOfAux t'
+
+/- bind : (x : m α) → (f : (α → m α))
+   unpacks the term from the monad x and applies
+   f to it. bind2 and bind3 are versions of bind where
+   f is binary and ternary respectively, with the
+   arguments reordered. -/
+def bind2 {m : Type → Type} [Monad m] {α β γ : Type}
+  (f : α → β → m γ) (a : m α) (b : m β) : m γ :=
+    a >>= fun a' => b >>= fun b' => f a' b'
+
+def bind3 {m : Type → Type} [Monad m] {α β γ δ : Type}
+  (f : α → β → γ → m δ) (a : m α) (b : m β) (c : m γ) : m δ :=
+    a >>= fun a' => b >>= fun b' => c >>= fun c' => f a' b' c'
+
+-- application of term to term
+def mkAppAux : term → term → Option term :=
+  fun t₁ t₂ =>
+    sortOf t₁ >>= fun s₁ =>
+    sortOf t₂ >>= fun s₂ =>
+    match s₁ with
+    | arrow s₂ _ => t₁ • t₂
+    | _ => none
+
+-- binary and n-ary application
+def mkApp : Option term → Option term → Option term := bind2 mkAppAux
 
 end term
 
