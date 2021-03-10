5f139836258d55331eff9084c2c1cefd50abf799
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-03-10 16:52:45 -0300

    updating euf to only have curried cong

--- lean/euf.lean
+++ lean/euf.lean
@@ -9,17 +9,13 @@ namespace eufRules
 /------------------ congruence ------------------/
 
 /-
-            t₁ = t₂      t₁ ≠ t₂   
+            t₁ = t₂      t₁ ≠ t₂
 -----refl   -------symm  -------negSymm
 t = t       t₂ = t₁      t₂ ≠ t₁
 
-t₁ = t₂  t₂ = t₃          t₁ = t₂
-----------------trans   -----------cong
-       t₁ = t₃          f t₁ = f t₂
-
-f₁ = f₂  t₁ = t₂
------------------congHO
-  f₁ t₁ = f₂ t₂ 
+t₁ = t₂  t₂ = t₃         f₁ = f₂  t₁ = t₂
+----------------trans   ------------------cong
+       t₁ = t₃          f₁ t₁ = f₂ t₂
 -/
 constant refl {t : option term} : thHolds $ mkEq t t
 
@@ -32,10 +28,7 @@ constant negSymm : Π {t₁ t₂ : option term},
 constant trans : Π {t₁ t₂ t₃ : option term},
   thHolds (mkEq t₁ t₂) → thHolds (mkEq t₂ t₃) → thHolds (mkEq t₁ t₃)
 
-constant cong : Π {t₁ t₂ : option term} (f : term),
-  thHolds (mkEq t₁ t₂) → thHolds (mkEq (mkApp f t₁) (mkApp f t₂))
-
-constant congHO : Π {f₁ t₁ : option term} {f₂ t₂ : option term},
+constant cong : Π {f₁ t₁ : option term} {f₂ t₂ : option term},
   thHolds (mkEq f₁ f₂) → thHolds (mkEq t₁ t₂) →
         thHolds (mkEq (mkApp f₁ t₁) (mkApp f₂ t₂))
 
