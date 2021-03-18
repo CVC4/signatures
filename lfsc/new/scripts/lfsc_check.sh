1308c7d0225bd2170f8b26346bfcf7ec5a2e8ab2
ajreynol <andrew.j.reynolds@gmail.com>
2021-03-18 10:35:49 -0500

    Improve

--- lfsc/new/scripts/lfsc_check.sh
+++ lfsc/new/scripts/lfsc_check.sh
@@ -2,7 +2,8 @@
 
 echo "=== Check with lfsc $@"
 
-SIGDIR=../lfsc/new/signatures
+SIGDIR=../signatures
+LFSCDIR=~/lfsc/git/cmake/src
 
 cat $@ | grep WARNING
 
@@ -10,7 +11,8 @@ CHECK=$(cat $@ | grep check)
 
 [ -z "$CHECK" ] && echo "; WARNING: Empty proof!!!"
 
-./lfscc $SIGDIR/core_defs.plf $SIGDIR/util_defs.plf $SIGDIR/theory_def.plf $SIGDIR/type_checking_programs.plf $SIGDIR/nary_programs.plf $SIGDIR/boolean_programs.plf $SIGDIR/boolean_rules.plf $SIGDIR/cnf_rules.plf $SIGDIR/equality_rules.plf $SIGDIR/arith_programs.plf $SIGDIR/arith_rules.plf $SIGDIR/strings_programs.plf $SIGDIR/strings_rules.plf $SIGDIR/quantifiers_rules.plf $@ >& temp-lfsc.txt
+$LFSCDIR/lfscc $SIGDIR/core_defs.plf $SIGDIR/util_defs.plf $SIGDIR/theory_def.plf $SIGDIR/type_checking_programs.plf $SIGDIR/nary_programs.plf $SIGDIR/boolean_programs.plf 
+$SIGDIR/boolean_rules.plf $SIGDIR/cnf_rules.plf $SIGDIR/equality_rules.plf $SIGDIR/arith_programs.plf $SIGDIR/arith_rules.plf $SIGDIR/strings_programs.plf $SIGDIR/strings_rules.plf $SIGDIR/quantifiers_rules.plf $@ >& temp-lfsc.txt
 
 # recover macros for applications of arity 1,2,3, and simplify builtin syntax for constants
 #sed -i 's/(f_ite [^ \)]*)/f_ite/g' temp-lfsc.txt
