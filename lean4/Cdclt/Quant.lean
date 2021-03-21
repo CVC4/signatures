4440f908fb9dffdefbffc9f8e5dd337a2f1e4f36
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-03-21 18:55:38 -0300

    improving

--- lean4/Cdclt/Quant.lean
+++ lean4/Cdclt/Quant.lean
@@ -8,25 +8,24 @@ namespace quantRules
 
 def t := val (value.bitvec [true, false])
 
-def substitute (body : term) (v : Nat) (t : term) : Option term :=
-  match body with
-  -- replace each term in application
-  | f • t₁ => substitute f v t >>= λ fs =>
-              substitute t₁ v t >>= λ t₁s => fs • t₁s
-  -- if found variable, replace by instantiation term *if types match*,
-  -- otherwise fail
-  | const v' s => if v ≠ v' then body
-                  else
-                    s >>= λ s' =>
-                    sortOf t >>= λ st =>
-                    if s' = st then t else none
-  -- if finds shadowing, break
-  | qforall v' body' => if v = v' then none
-                        else mkForall v' (substitute body' v t)
-  -- otherwise no change
-  | _ => body
+def substitute (v : Nat) (t : term) : term → Option term
+-- replace each term in application
+| f • t₁ => substitute v t f >>= λ fs =>
+            substitute v t t₁ >>= λ t₁s => fs • t₁s
+-- if found variable, replace by instantiation term *if types match*,
+-- otherwise fail
+| body@(const v' s) => if v ≠ v' then body
+                else
+                  s >>= λ s' =>
+                  sortOf t >>= λ st =>
+                  if s' = st then t else none
+-- if finds shadowing, break
+| qforall v' body' => if v = v' then none
+                      else mkForall v' (substitute v t body')
+-- otherwise no change
+| body => body
 
 axiom instForall : ∀ {v : Nat} {body : term} (t : term),
-  thHolds (qforall v body) → thHolds (substitute body v t)
+  thHolds (qforall v body) → thHolds (substitute v t body)
 
 end quantRules
