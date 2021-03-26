c0b2852e7baa518450a44ddeba8a9114dc43615b
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-03-26 00:11:37 -0300

    adding a term evaluator (just for the Boolean operators for now)

---
+++ lean4/Cdclt/TermEval.lean
@@ -0,0 +1,73 @@
+import Cdclt.Term
+import Cdclt.BV
+
+open proof
+open proof.sort proof.term
+
+namespace proof
+
+namespace term
+
+partial def termEval (ot : Option term) : Option term :=
+  ot >>= λ t =>
+  match t with
+  | top => top
+  | bot => bot
+  | term.not t₁ =>
+    do let t₁' ← termEval t₁
+       match t₁' with
+       | top => bot
+       | bot => top
+       | _ => t
+  | and t₁ t₂ =>
+    do let t₁' ← termEval t₁
+       let t₂' ← termEval t₂
+       match t₁', t₂' with
+       | top, top => top
+       | bot, _ => bot
+       | _, bot => bot
+       | _, _ => t
+  | or t₁ t₂ =>
+    do let t₁' ← termEval t₁
+       let t₂' ← termEval t₂
+       match t₁', t₂' with
+       | bot, bot => bot
+       | top, _ => top
+       | _, top => top
+       | _, _ => t
+  | implies t₁ t₂ =>
+    do let t₁' ← termEval t₁
+       let t₂' ← termEval t₂
+       match t₁', t₂' with
+       | bot, _ => top
+       | top, bot => bot
+       | top, top => top
+       | _, _ => t
+  | xor t₁ t₂ =>
+    do let t₁' ← termEval t₁
+       let t₂' ← termEval t₂
+       match t₁', t₂' with
+       | bot, top => top
+       | top, bot => top
+       | top, top => bot
+       | bot, bot => bot
+       | _, _ => t
+  | eq t₁ t₂ => if t₁ = t₂ then top else bot
+  | bIte c t₁ t₂ =>
+    do let c' ← termEval c
+       match c' with
+       | top => termEval t₁
+       | bot => termEval t₂
+       | _ => t
+  | _ => t
+
+#check termEval (mkAnd top bot)
+
+#eval termEval (mkAnd top bot)
+
+#eval termEval $ bblastBvSlt (val (value.bitvec [true, true, true, true]) (bv 4))
+  (val (value.bitvec [false, false, false, false]) (bv 4))
+
+end term
+
+end proof
